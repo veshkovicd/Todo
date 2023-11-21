12012db41670a87dcec773ebbd1099d3bd8575fc
Dushko Veshkovic <92746187+veshkovicd@users.noreply.github.com>
2023-11-21 11:03:23 +0100

    delete functionalities (#3)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputItem from './InputItem';
 
-const TodoList = ({ list, inputTask, handleInputChange, handleOnEnterPressed, handleSaveTodoItem }) => {
+const TodoList = ({
+    list,
+    inputTask,
+    handleInputChange,
+    handleOnEnterPressed,
+    handleSaveTodoItem,
+    handleDeleteAll,
+    handleDeleteTodoItem,
+    handleDeleteSelectedTodos,
+    selectedItemsArray,
+    setSelectedItemsArray
+}) => {
+    const [hoveredId, setHoveredId] = useState(null);
+
+    const handleItemCheckboxChange = (id) => {
+        const updatedSelectedItems = selectedItemsArray.includes(id)
+            ? selectedItemsArray.filter(itemId => itemId !== id)
+            : [...selectedItemsArray, id];
+        setSelectedItemsArray(updatedSelectedItems);
+    };
+
     return (
         <div>
             <h1>Todo List</h1>
@@ -11,12 +31,24 @@ const TodoList = ({ list, inputTask, handleInputChange, handleOnEnterPressed, ha
                 onKeyDown={handleOnEnterPressed}
                 onClick={handleSaveTodoItem}
             />
+            <button onClick={handleDeleteAll}>Delete all</button>
+            <button onClick={handleDeleteSelectedTodos}>Delete Selected</button>
             <ul>
-                {list.map((todo, index) => (
-                    <li key={index}>{todo}</li>
+                {list.map((todo) => (
+                    <li key={todo.id} onMouseEnter={() => setHoveredId(todo.id)} onMouseLeave={() => setHoveredId(null)}>
+                        <input
+                            type="checkbox"
+                            checked={selectedItemsArray.includes(todo.id)}
+                            onChange={() => handleItemCheckboxChange(todo.id)}
+                        />
+                        <span>{todo.value}</span>
+                        {hoveredId === todo.id && <button onClick={() => handleDeleteTodoItem(todo.id)}>X</button>}
+                    </li>
                 ))}
             </ul>
+            
         </div>
     );
-}
+};
+
 export default TodoList;
